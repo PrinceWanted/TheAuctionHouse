a375ca2037910cf0ebb730219790b3b9a47f794a
john0132 <57411754+john0132@users.noreply.github.com>
2021-01-15 22:45:41 +0200

    Revert "Revert "Interfaces update""
    This reverts commit 5213c9b8b7e51e97594e2e3103f255a7a67ad62f.

--- Server/src/theauctionhouse/SellerInterface.java
+++ Server/src/theauctionhouse/SellerInterface.java
@@ -11,4 +11,7 @@ public interface SellerInterface extends Remote,UserInterface {
     public void returnDTO(SellerDTO d) throws RemoteException;
 
     public void postProduct(Product r) throws RemoteException;
+
+    @Override
+    public Seller login (String name,String pass) throws RemoteException;
 }
